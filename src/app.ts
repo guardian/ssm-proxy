e9efb8540e08b76ef2cc2430b9d2f362ede36366
Michael Barton <michael.barton@guardian.co.uk>
2019-07-19 17:38:23 +0100

    Lookup region and profile using Prism

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,29 @@
 #!/usr/bin/env node
+import fetch from 'node-fetch';
 
 const instanceId = process.argv[2];
 
+async function lookupInstance(instanceId: string): Promise<{ region: string, profile: string }> {
+    const { data } = await fetch("https://prism.gutools.co.uk/instances?instanceName=" + instanceId).then(r => r.json());
+
+    if(!data.instances || data.instances.length === 0) {
+        return Promise.reject("instanceId does not exist");
+    }
+
+    const profile  = data.instances[0].meta.origin.accountName;
+    const region = data.instances[0].region;
+
+    return { region, profile };
+}
+
 // TODO MRB:
-//  - Look up instance id in prism to get profile (account name) and region
 //  - Provision SSH keys on the machine (copying ssm-scala)
 //  - Shell to ssm create-session and connect stdin as proxy
 //  - Bonus extra credit
 //      - SSH into tags (eg ssh aws:investigations,pfi-worker,rex)
 
-console.error("I will eventually connect to " + instanceId);
+lookupInstance(instanceId).then(({ profile, region }) => {
+    console.error(`I will eventually connect to ${instanceId} in ${region} using ${profile} credentials`);
+}).catch(err => {
+    console.error(err);
+});
